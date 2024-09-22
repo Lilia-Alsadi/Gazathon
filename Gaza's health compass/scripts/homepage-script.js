60126cebf8d7e00aa784b7f4690db0896ee1e228
Lilia-Alsadi <129851225+Lilia-Alsadi@users.noreply.github.com>
2024-09-22 08:58:36 -0700

    Add files via upload

---
+++ Gaza's health compass/scripts/homepage-script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+const chatInput = document.getElementById("Text");
+const sendChatBtn = document.getElementById("enter-button");
+const chatBox = document.querySelector(".chatbox");
+const chatboxtoggler = document.querySelector(".chatbox-toggler");
+const chatBot = document.querySelector(".chatBot");
+let userMessage;
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<i class="fa-solid fa-hippo"></i><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); 
+    
+    if (!userMessage) return;
+
+    chatBox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatInput.value = ""; 
+
+    setTimeout(()=>{
+        chatBox.appendChild(createChatLi("Thinking...","incoming"));
+    },600)
+
+}
+sendChatBtn.addEventListener("click", handleChat);
+chatboxtoggler.addEventListener("click", () => {
+    chatBot.classList.toggle("visible"); // Toggle a class for visibility
+});
+});
+
+
+
+
+
+
+
