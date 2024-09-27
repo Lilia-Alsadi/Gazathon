2c4b8d40ddbd11075b7ac1b0966382b46fd2d26c
Lilia-Alsadi <129851225+Lilia-Alsadi@users.noreply.github.com>
2024-09-27 07:32:02 -0700

    Add files via upload

---
+++ final ghazathon website _/scripts/newssearch.js
@@ -0,0 +1,225 @@
+let info = {
+    data: [
+        {
+            Title: "Flooding Threatens Public Health in Gaza",
+            image: "../imgs/news/news1.jpeg",
+            category1: "all",
+            category2: "all",
+            id: "1",
+            Description: "Assessing the health risks linked to flooding in Gaza.",
+            Content: `The 2023 flood risk assessment has raised alarming concerns regarding the safety of internally displaced persons (IDPs) in Gaza. Many of these individuals are currently residing near identified flooding hotspots, putting them at significant risk, particularly given the extensive devastation caused by the ongoing conflict. As the rainy season approaches, the Health and Water, Sanitation, and Hygiene (WASH) Clusters have identified several critical public health threats associated with potential flooding.<br>**Injuries and Drowning:** Rapid flooding poses an immediate danger, especially in low-lying areas where water can accumulate quickly, leading to dangerous conditions for those residing nearby. The risk of injuries and drowning becomes heightened in such scenarios, particularly among vulnerable populations, including children.
+
+            <br>**Disruption of Healthcare Services:** The current infrastructure is particularly concerning, with 47% of health service points (114 out of 242) situated near flooding hotspots. This proximity exposes these facilities to potential damage during flooding events, jeopardizing access to essential health services. Such disruptions could exacerbate existing health issues, leaving many without necessary medical care.
+
+            <br>**Waterborne Diseases:** Flooding can lead to the contamination of water supplies with sewage, significantly increasing the risk of waterborne diseases such as cholera and dysentery. The potential for outbreaks becomes a serious public health concern, particularly in areas already facing limited healthcare resources.
+
+            <br>**Vector-borne Diseases:** Stagnant water pools created by flooding can become breeding grounds for mosquitoes, elevating the risk of vector-borne diseases, including West Nile Fever and malaria. This presents an additional layer of health threat to a population already grappling with numerous challenges.
+
+            <br>**Sanitation Issues:** The flooding can severely damage sanitation infrastructure and disrupt waste management systems, including the management of both medical and general waste. The breakdown of these systems significantly raises the risk of disease transmission. Furthermore, temporary shelters and tents, often utilized by displaced families, are vulnerable to flooding, exacerbating health risks for those already in precarious living conditions.
+
+            <br>**Snakebites:** The risk of snakebites also escalates in areas like Deir Al-Balah, where farming activities can increase encounters with snakes, particularly during and after flooding events. This additional hazard requires immediate attention and preventive measures to protect vulnerable populations.
+
+            <br>**Mental Health and Long-Term Health Impact:** The psychological toll of flooding cannot be overlooked. The stress and disruption caused by flooding can intensify mental health issues and chronic health conditions. Exposure to mould, chemicals, and other environmental hazards following a flood can lead to respiratory problems and other long-term health complications.
+
+            <br>The Health and WASH Clusters are urging for an immediate and sustained ceasefire to initiate the reconstruction process before the winter season. Without urgent intervention and support, the health risks associated with flooding will continue to escalate, further threatening the lives and well-being of the population in Gaza. The time for action is now; public health and safety depend on it.`
+        }
+        ,
+        {
+            Title: "Polio Returns to Gaza: Where Else Has the Virus Re-emerged?",
+            image: "../imgs/news/news2.webp",
+            category1: "all",
+            category2: "0-18",
+            id: "2",
+            Description: "The resurgence of polio in Gaza after 25 years.",
+            Content: `Polio has made a comeback in the Gaza Strip after 25 years, forcing the United Nations and local health authorities to launch a vaccination campaign in the Palestinian enclave ravaged by 11 months of war. 
+
+        About 640,000 children under 10 years old are expected to receive oral drops of the polio vaccine amid limited pauses in fighting, with campaigners noting that Israel’s continued destruction of water and waste management infrastructure in Gaza is aiding the spread of the highly contagious disease. 
+
+        The disease, which causes paralysis of limbs primarily in children under five years old, had been nearly eradicated globally. However, it has re-emerged in several countries worldwide. Vaccine-derived cases occur when the weakened virus present in oral vaccines spreads and mutates sufficiently enough to cause infections. This typically happens over 12 to 18 months and primarily impacts people who were unvaccinated, did not complete their vaccinations, or for whom the vaccination did not work, according to Howard Forman, director of the Health Care Management Program at Yale University’s School of Public Health.
+
+        Health experts and campaigners have attributed the resurgence of polio to Israel’s destruction of Gaza’s health and sanitation infrastructure. The first case of polio in Gaza this century was detected in mid-August when a 10-month-old boy became partially paralysed after contracting the virus. This alarming case came just weeks after the UN health agency raised the alarm that poliovirus had been found in Gaza’s wastewater.
+
+        “If the Israeli government continues to block urgent aid and destroy water and waste management infrastructure, it will facilitate the spread of a disease that has been nearly eradicated globally,” stated Julia Bleckner, senior health and human rights researcher at Human Rights Watch. 
+
+        “Israel’s partners should press the government to lift the blockade immediately and ensure unfettered humanitarian access in Gaza to enable the timely distribution of vaccines to contain the unfolding polio outbreak.”`
+        },
+        {
+            Title: "Pregnant Women and Newborns: The Most Vulnerable in Conflict",
+            image: "../imgs/news/news3.jpeg",
+            category1: "women",
+            category2: "16-60",
+            id: "3",
+            Description: "The plight of pregnant women and newborns amid escalating violence in Gaza.",
+            Content: `The escalation of violence in the Gaza Strip is evolving rapidly, with more than half of the population forced to flee their homes. Many are seeking refuge in UN shelters, schools, hospitals, or with relatives, compounding the humanitarian crisis.
+
+        The United Nations Population Fund (UNFPA) is critically concerned for women and girls in Gaza, particularly the estimated 50,000 pregnant women. Among these, over 5,000 are expected to deliver in the next month. Unfortunately, these women are cut off from safe delivery services as hospitals come under attack and the health system unravels. Access to life-saving medicines, fuel, and electricity is running low, endangering both maternal and newborn health.
+
+        In the West Bank, the situation is similarly dire, with around 73,000 women currently pregnant and more than 8,000 expected to give birth in the next month. The ongoing violence poses a significant threat to their health and safety as tensions escalate.
+
+        In response to this critical situation, UNFPA is working alongside partners to provide essential life-saving services. This includes addressing the availability of out-of-stock reproductive health supplies for pregnant women and new mothers. The organization’s network of midwives will be activated to offer support, ensuring that connections with hospital-level specialists remain intact.
+
+        Additionally, UNFPA is supporting hotlines to provide vital information and counselling services to women and youth, aiming to alleviate some of the distress and uncertainty faced by families during these challenging times. The urgent need for comprehensive support for pregnant women and newborns cannot be overstated as the conflict continues to disrupt essential health services.`
+        },
+
+        {
+            Title: "Gaza Crisis: Older People Face a Catastrophic Situation",
+            image: "../imgs/news/news4.jpeg",
+            category1: "all",
+            category2: "60+",
+            id: "4",
+            Description: "The devastating impact of the ongoing conflict on older adults in Gaza.",
+            Content: `The ongoing conflict in Gaza has devastated every aspect of life, leaving older people among the most vulnerable in this humanitarian crisis. At 75 years old, Dr. Yehia Abed, director of operations for Juzoor, a non-governmental organization in Gaza, has witnessed more than 20 wars, yet he states that he has never seen anything as dire as the current situation.
+    
+                Dr. Abed highlights that traditional respect for older individuals is now challenged by the dire scarcity of food and the widespread destruction of homes. "We always treated our parents and grandparents with ultimate respect; good food and comfort were prioritized for them. Now, that respect has had to be overlooked due to food scarcity. Older people prefer to give their food to children; they want peace and security for their children, not for themselves," he explains.
+    
+                Many older people have been forced to flee their homes, relying on hand-drawn carriages for transportation, often facing bombings during their escape. Tragically, children often plead with their elderly parents to leave for safety, only to discover them injured or deceased upon their return. The emotional toll of such losses can be overwhelming for families.
+    
+                Despite these hardships, Juzoor remains steadfast in its mission, providing essential training on health issues and supplying first aid kits to nurses and doctors in shelters. "Initially, we believed this would be a short-term fix, but it has lasted far longer than we anticipated," Dr. Abed noted. With support from international organizations like HelpAge International, Juzoor oversees 70 medical facilities across shelters throughout Gaza.
+    
+                These facilities are a critical lifeline, staffed by doctors, nurses, midwives, and psychosocial specialists. However, they face significant challenges in meeting the critical medication needs of older individuals, particularly those with chronic diseases. "We divide cases into acute and those that can wait, ensuring each shelter is equipped with essential medical supplies. In the absence of electricity, we rely on manual pumps to operate medical machines," Dr. Abed explained.
+    
+                The resilience of the community has been a source of strength, with many individuals donating what little they have. However, the needs of older people far exceed available resources. "We only cover 5% of older people's needs. When we do manage to find a wheelchair for an older person, they often ask us to give it to one of their children or grandchildren who have been injured or had a leg amputated," he said.
+    
+                As the conflict endures, Juzoor's support has shifted to a life-saving approach. They have distributed cash and clothing to older individuals but have had to raise the minimum age for assistance from 60 to 65 due to overwhelming needs. Dr. Abed and his team have searched damaged pharmacies and storage facilities to acquire necessary medications and supplies.
+    
+                The ongoing food scarcity has led to widespread malnutrition in Gaza. "I’m a living example," Dr. Abed admitted. "I’ve lost 20 kilos since the war began. This isn’t about isolated cases of starvation; the entire population of Gaza is affected. Fresh vegetables or meat haven’t been seen in months, and food prices have skyrocketed. We have not seen a single food truck for a long time. The impact of malnutrition on older people is severe, and their chances of recovery are very low."
+    
+                Dr. Abed describes the situation in Gaza as a humanitarian crisis of unprecedented proportions, emphasizing that every aspect of life has been devastated by the conflict, leaving older people particularly vulnerable.`
+        },
+
+        {
+            Title: "How Israel is Shrinking Gaza’s ‘Safe Zones’",
+            image: "../imgs/news/news5.webp",
+            category1: "all",
+            category2: "16-60",
+            id: "5",
+            Description: "The increasing designation of areas in Gaza as unsafe for civilians amid ongoing conflict.",
+            Content: `As of Monday, 83 percent of the Gaza Strip has been marked unsafe for Palestinian civilians by the Israeli military. This significant portion of the beleaguered enclave has either been designated as a “no-go zone” or evacuation orders have been issued for residents, according to the United Nations Office for the Coordination of Humanitarian Affairs (OCHA).
+    
+                The remaining 17 percent of Gaza is not subject to evacuation orders; however, residents report that no place is safe as Israeli attacks have persisted throughout the Strip for more than nine months, resulting in over 39,000 Palestinian deaths.
+    
+                Since the onset of Israel’s latest assault on October 7, nearly all Palestinians in Gaza have been displaced from their homes, with many experiencing multiple displacements. Entire neighborhoods in northern Gaza, where the initial offensive began, have been completely destroyed, while the so-called ‘safe zones’ in southern Gaza continue to shrink and deteriorate.
+    
+                On Monday, the Israeli military issued an evacuation order for residents in the eastern part of Khan Younis, directing them to relocate further west into the al-Mawasi evacuation zone. The military claimed that the Palestinian group Hamas was embedded in that area, further complicating the safety of civilians.
+                `
+        },
+
+        {
+            Title: "Israel’s Siege Now Blocks 83% of Food Aid Reaching Gaza",
+            image: "../imgs/news/news6.jpeg",
+            category1: "all",
+            category2: "16-60",
+            id: "6",
+            Description: "A drastic reduction in food, medicine, and other life-saving aid entering Gaza is causing a severe humanitarian crisis.",
+            Content: `New data reveals that 83% of the required food aid is being blocked from entering Gaza due to the ongoing Israeli siege. This obstruction is creating a devastating humanitarian disaster, with nearly the entire population facing hunger and disease. Almost half a million people, including 50,000 children, are at risk of starvation. The scale of aid obstruction has increased dramatically from 34% in 2023 to 83% in 2024.
+            
+                        The siege has blocked essential items such as food, medicine, fuel, and shelter materials for nearly a year. Insulin, blood supplies, and hygiene products have also seen drastic shortages, severely impacting medical care. Only 17 out of 36 hospitals remain partially functional, with just 1,500 operational hospital beds for over 2 million people. Tents for only 25,000 individuals have entered Gaza since May 2024, leaving 1.87 million people in need of shelter.
+            
+                        Aid agencies and humanitarian workers face severe challenges, with more than 40,000 Palestinians and nearly 300 aid workers killed since October 2023. Despite the critical need, Israel’s control over aid distribution and its destruction of vital infrastructure has made the situation unbearable.
+            
+                        International aid organizations are urging governments to pressure Israel into lifting the siege and allowing aid to flow freely. Ahead of the UN General Assembly, they are calling for an immediate ceasefire, an arms embargo, and compliance with international law to end the blockade and prevent further humanitarian catastrophe.`
+        },
+
+    ],
+};
+
+
+for (let i of info.data) {
+    let card = document.createElement("div");
+    card.classList.add("card", i.category1, i.category2, "hide");
+
+    // Create anchor element
+    let link = document.createElement("a");
+    link.setAttribute("href", "../HTML/news.html");  // Use the link from the data
+    link.setAttribute("target", "_blank");  // Open the link in a new tab (optional)
+    link.classList.add("video-link");
+
+    link.setAttribute("data-id", i.id);  // Store the id in the link
+
+    let imgcontainer = document.createElement("div");
+    imgcontainer.classList.add("image-container");
+
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgcontainer.appendChild(image);
+
+    link.appendChild(imgcontainer);
+
+    let container = document.createElement("div");
+    container.classList.add("container");
+
+    let title = document.createElement("h5");
+    title.classList.add("titleOfInfo");
+    title.innerText = i.Title.toUpperCase();
+
+    container.appendChild(title);
+    link.appendChild(container);  // Add container to the link
+
+    card.appendChild(link);  // Append the entire link (with image and title) to the card
+
+    document.getElementById("info").appendChild(card);
+}
+
+
+function filterinfo(value) {
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        if (value.toUpperCase() == button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+
+    let elements = document.querySelectorAll(".card");
+    elements.forEach((element) => {
+        if (value == "all") {
+            element.classList.remove("hide");
+        }
+        else {
+            if (element.classList.contains(value)) {
+                element.classList.remove("hide");
+            }
+            else {
+                element.classList.add("hide");
+            }
+        }
+    })
+}
+
+
+document.getElementById("search").addEventListener("click", () => {
+    let searchInput = document.getElementById("search-input").value; // Use quotes for ID
+    let elements = document.querySelectorAll(".titleOfInfo");
+    let cards = document.querySelectorAll(".card");
+
+    elements.forEach((element, index) => {
+        if (element.innerText.includes(searchInput.toUpperCase())) {
+            cards[index].classList.remove("hide");
+        }
+        else {
+            cards[index].classList.add("hide");
+        }
+    })
+});
+
+window.onload = () => {
+    filterinfo("all");
+}
+
+document.querySelectorAll('.video-link').forEach(link => {
+    link.addEventListener('click', function (event) {
+        const newsID = event.currentTarget.dataset.id;  // Get the id from data-id
+        const selectedNews = info.data.find(article => article.id === newsID);
+
+        // Store the selected article data in sessionStorage
+        if (selectedNews) {
+            localStorage.setItem('Title', selectedNews.Title);
+            localStorage.setItem('Description', selectedNews.Description);
+            localStorage.setItem('Content', selectedNews.Content);
+            localStorage.setItem('img', selectedNews.image);
+        }
+    });
+});
+
